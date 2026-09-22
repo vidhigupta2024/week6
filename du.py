857bd828bfdfbafb90948c765629e6377ef3f546
vidhigupta2024 <vidhi.gupta.2024@smu.edu.sg>
2026-09-22 18:38:42 +0800

    Fix indentation for fee calculation and total owed update in DuckFine class

--- du.py
+++ du.py
@@ -13,6 +13,6 @@ class DuckFine:
         fee = chargeable * self.DAILY_FEE
         if deluxe:
             fee *= 2
-        fee = min(fee, self.MAX_FEE)
-        self.total_owed += fee
+            fee = min(fee, self.MAX_FEE)
+            self.total_owed += fee
         return fee
